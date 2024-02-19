92cb8e418d4228defd0bf695e4ee72e85ba659c8
Dieguidev <diegogaraycullas@gmail.com>
2024-02-18 22:52:46 -0500

    auth.controller.js: en login se mejora codigo para verificar si email existe, si usuario esta activo y si el password es el correcto

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,9 +1,45 @@
 const { response, request } = require('express');
+const bcrypt = require('bcryptjs');
 
-const login = (req = request, res = response) => {
-  res.json({
-    msg: 'Login',
-  });
+const User = require('../models/user.model');
+
+const login = async (req = request, res = response) => {
+  const { email, password } = req.body;
+  try {
+    //verificar si el email existe
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({
+        msg: 'Usuario / Password no son correctos - email',
+      });
+    }
+
+    //verificar si el usuario esta activo
+    if (!user.isActive) {
+      return res.status(400).json({
+        msg: 'Usuario / Password no son correctos - status: false',
+      });
+    }
+
+    //verificar la contraseña
+    const validPassword = bcrypt.compareSync(password, user.password);
+    if (!validPassword) {
+      return res.status(400).json({
+        msg: 'Usuario / Password no son correctos - password',
+      });
+    }
+
+    //generar el JWT
+
+    res.json({
+      msg: 'Login',
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      msg: 'Hable con el administrador',
+    });
+  }
 };
 
 module.exports = {
