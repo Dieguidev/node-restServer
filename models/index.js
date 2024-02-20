e9cd4e96f14ce7e2780a549f6aa0234e2a693b0f
Dieguidev <diegogaraycullas@gmail.com>
2024-02-19 20:55:51 -0500

    categorie.model.js: se genera el modelo de categorias

---
+++ models/index.js
@@ -0,0 +1,9 @@
+const Categorie = require('./categorie.model');
+const Role = require('./role.model');
+const User = require('./user.model');
+
+module.exports = {
+  Categorie,
+  Role,
+  User,
+};
